ec9c9c995ffde0b930b2cfda835bc019320ea40a
nenertiy <nenertiy@gmail.com>
2024-10-30 09:30:53 +0300

    skeleton cards

---
+++ src/components/ProductCard/SkeletonProductCard.tsx
@@ -0,0 +1,7 @@
+import styles from "./SkeletonProductCard.module.scss";
+
+const SkeletonProductCard = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default SkeletonProductCard;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -6,12 +6,13 @@ import Input from "../../components/Input/Input";
 import { useQuery } from "@tanstack/react-query";
 import { addToCart, fetchProducts } from "../../utils/api";
 import { useAuthStore } from "../../store/authStore";
+import SkeletonProductCard from "../../components/ProductCard/SkeletonProductCard";
 
 const Menu: FC = () => {
   const [searchValue, setSearchValue] = useState<string>("");
   const userId = useAuthStore((state) => state.userId);
 
-  const { data, isSuccess } = useQuery({
+  const { data, isSuccess, isLoading } = useQuery({
     queryKey: ["products", searchValue],
     queryFn: () => fetchProducts(searchValue),
   });
@@ -30,35 +31,36 @@ const Menu: FC = () => {
     }
   };
 
+  const arr = [...Array(5)];
+
   return (
     <div className={styles.container}>
       <div className={styles.input}>
-        <Input
-          value={searchValue}
-          handleChange={handleChange}
-        />
+        <Input value={searchValue} handleChange={handleChange} />
       </div>
       <div className={styles.list}>
-        {isSuccess &&
-          data?.map(
-            (product: {
-              id: string;
-              name: string;
-              img: string;
-              price: number;
-              productCategory: { name: string };
-            }) => (
-              <ProductCard
-                key={product.id}
-                id={product.id}
-                name={product.name}
-                img={product.img}
-                price={product.price}
-                category={product?.productCategory?.name}
-                handleAddToCart={() => handleAddToCart(product.id)}
-              />
-            )
-          )}
+        {isLoading
+          ? arr.map((_, index) => <SkeletonProductCard key={index} />)
+          : isSuccess &&
+            data?.map(
+              (product: {
+                id: string;
+                name: string;
+                img: string;
+                price: number;
+                productCategory: { name: string };
+              }) => (
+                <ProductCard
+                  key={product.id}
+                  id={product.id}
+                  name={product.name}
+                  img={product.img}
+                  price={product.price}
+                  category={product?.productCategory?.name}
+                  handleAddToCart={() => handleAddToCart(product.id)}
+                />
+              )
+            )}
       </div>
       <div className={styles.pagination}></div>
     </div>
